126ead26249aae337ddee4037d983d01fbbd030e
Mohammad_jaffer_alsaadi <Mohammadjaffrsaadix@gmail.com>
2025-09-29 00:12:16 +0500

    update

--- app/Http/Controllers/API/Auth/UserAuthController.php
+++ app/Http/Controllers/API/Auth/UserAuthController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\api\Auth;
+namespace App\Http\Controllers\API\Auth;
 
 use App\Mail\OtpMail;
 use App\Models\User;
--- app/Http/Controllers/Web/CategoryController.php
+++ app/Http/Controllers/Web/CategoryController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\web;
+namespace App\Http\Controllers\Web;
 
 use App\Classes\ApiResponseClass;
 use App\Models\Category;
